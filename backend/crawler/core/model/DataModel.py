df890afe1cba41f23f90aab217ed1405798cb879
bodommer <andrej.jurco@me.com>
2022-04-15 15:18:32 +0200

    Add pydocs to data model classes

--- backend/crawler/core/model/DataModel.py
+++ backend/crawler/core/model/DataModel.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class WebsiteRecord(models.Model):
+    """
+    Represents a single website crawling record - instructions for crawling.
+    """
     STATUS = ((1, 'Active'), (0, 'Inactive'))
     url = models.CharField(max_length=256)
     label = models.CharField(max_length=64)
@@ -11,11 +14,17 @@ class WebsiteRecord(models.Model):
 
 
 class Tag(models.Model):
+    """
+    Represents a single :class: `WebsiteRecord` tag.
+    """
     tag = models.CharField(max_length=64)
     website_record = models.ForeignKey(WebsiteRecord, on_delete=models.CASCADE)
 
 
 class Execution(models.Model):
+    """
+    Represents a single web crawler execution of the :class: `WebsiteRecord`.
+    """
     title = models.CharField(max_length=72)
     url = models.CharField(max_length=2048)
     crawl_time = models.DateTimeField()
@@ -23,16 +32,26 @@ class Execution(models.Model):
 
 
 class ExecutionLink(models.Model):
+    """
+    A link visited during an :class: `Execution`.
+    """
     url = models.CharField(max_length=2048)
     execution = models.ForeignKey(Execution, on_delete=models.CASCADE)
 
 
 class Node(models.Model):
+    """
+    A class object for a web map resulting from an :class: `Execution`.
+    Only one graph is stored per every :class: `WebsiteRecord` (the latest one).
+    """
     url = models.CharField(max_length=2048)
     domain = models.CharField(max_length=2048)
     website_record = models.ForeignKey(WebsiteRecord, on_delete=models.CASCADE)
 
 
 class Edge(models.Model):
+    """
+    A single edge between two :class: `Node` objects in the website graph.
+    """
     source = models.ForeignKey(Node, on_delete=models.CASCADE, related_name='source_node')
     target = models.ForeignKey(Node, on_delete=models.CASCADE, related_name='target_node')

685c08c3cd74867d5faa5c8b47d07711e348db03
Michal Jurco <michal.jurco@getmanta.com>
2022-04-15 17:24:51 +0200

    FE: cleanup

--- frontend/crawler/src/index.tsx
+++ frontend/crawler/src/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Core from './Core';
+import Sites from './Sites';
+import Dashboard from './Dashboard';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Core />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/home" element={<Dashboard />} />
+        <Route path="/sites" element={<Sites />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
